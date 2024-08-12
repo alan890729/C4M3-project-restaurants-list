e00071ccb2035370447a30cf47958a7c27df8012
alan890729 <alan890729@gmail.com>
2024-08-12 21:17:33 +0800

    implement create, read, update restaurant

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
-const restaurants = require('./public/jsons/restaurant.json').results
+const { Op } = require('sequelize')
+// const restaurants = require('./public/jsons/restaurant.json').results
+const db = require('./models')
+const Restaurant = db.Restaurant
 const app = express()
 const port = 3000
 
@@ -18,69 +21,142 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
-    res.render('restaurants', { restaurants })
+    return Restaurant.findAll({
+        raw: true
+    }).then((restaurants) => {
+        res.render('restaurants', { restaurants })
+    }).catch((err) => {
+        res.status(422).json(err)
+    })
 })
 
 app.get('/restaurants/new', (req, res) => {
     const restaurantCategories = []
-    
-    restaurants.forEach(restaurant => {
-        if (!restaurantCategories.some(category => category === restaurant.category)) {
-            restaurantCategories.push(restaurant.category)
+
+    return Restaurant.findAll(
+        {
+            attributes: ['category'],
+            raw: true
         }
+    ).then((data) => {
+        data.forEach(element => {
+            if (!restaurantCategories.some(category => element.category === category)) {
+                restaurantCategories.push(element.category)
+            }
+        })
+        res.render('new-restaurant', { categories: restaurantCategories })
     })
-    console.log(restaurantCategories)
-    res.render('new-restaurant', { categories: restaurantCategories })
 })
 
 app.post('/restaurants', (req, res) => {
-    res.send('processing incoming data, will soon create new restaurant data in database')
+    const body = req.body
+    body.rating = Number(body.rating)
+
+    return Restaurant.create(body).then((results) => {
+        res.redirect('/restaurants')
+    }).catch((err) => {
+        res.status(422).json(err)
+    })
 })
 
 app.get('/restaurants/:id', (req, res) => {
     const id = req.params.id
-    const restaurant = restaurants.find(restaurant => restaurant.id.toString() === id)
-    res.render('detail', { restaurant })
+
+    return Restaurant.findByPk(
+        Number(id),
+        {
+            raw: true
+        }
+    ).then((restaurant) => {
+        res.render('detail', { restaurant })
+    }).catch((err) => {
+        res.status(422).json(err)
+    })
 })
 
 app.get('/restaurants/:id/edit', (req, res) => {
     const id = req.params.id
-    const restaurant = restaurants.find(restaurant => restaurant.id.toString() === id)
-    const restaurantCategories = []
 
-    restaurants.forEach(restaurant => {
-        if (!restaurantCategories.some(category => category === restaurant.category)) {
-            restaurantCategories.push(restaurant.category)
-        }
+    return Promise.all(
+        [
+            Restaurant.findByPk(
+                Number(id),
+                {
+                    raw: true
+                }
+            ),
+            Restaurant.findAll(
+                {
+                    attributes: ['category'],
+                    where: {
+                        category: {
+                            [Op.not]: ''
+                        }
+                    },
+                    raw: true
+                }
+            )
+        ]
+    ).then((data) => {
+        const [restaurant, rawCategories] = data
+        const restaurantCategories = []
+        rawCategories.forEach(element => {
+            if (!restaurantCategories.some(category => element.category === category)) {
+                restaurantCategories.push(element.category)
+            }
+        })
+        res.render('restaurant-edit', { restaurant, categories: restaurantCategories })
+    }).catch((err) => {
+        res.status(422).json(err)
     })
-    console.log(restaurantCategories)
-    res.render('restaurant-edit', { restaurant, categories: restaurantCategories })
 })
 
 app.put('/restaurants/:id', (req, res) => {
     const id = req.params.id
     const body = req.body
-    res.send(`update restaurant: ${id}`)
+    body.rating = Number(body.rating)
+
+    return Restaurant.update(
+        body,
+        {
+            where: {
+                id: Number(id)
+            }
+        }
+    ).then((results) => {
+        res.redirect(`/restaurants/${id}`)
+    }).catch((err) => {
+        res.status(422).json(err)
+    })
+    
 })
 
 app.get('/search', (req, res) => {
     const keyword = req.query.keyword?.trim()
 
-    const matchedRestaurants = restaurants.filter(restaurant =>
-        Object.keys(restaurant).some(key => {
-            if (key === 'name' || key === 'category') {
-                return restaurant[key].toLowerCase().includes(keyword.toLowerCase())
-            }
-            return false
-        })
-    )
-
-    if (!matchedRestaurants.length) {
-        res.render('unmatched', { keyword })
-        return
-    }
-
-    res.render('restaurants', { restaurants: matchedRestaurants, keyword })
+    Restaurant.findAll(
+        {
+            where: {
+                [Op.or]: {
+                    name: {
+                        [Op.like]: `%${keyword}%`
+                    },
+                    category: {
+                        [Op.like]: `%${keyword}%`
+                    }
+                }
+            },
+            raw: true
+        }
+    ).then((data) => {
+        if (!data.length) {
+            res.render('unmatched', { keyword })
+        } else {
+            res.render('restaurants', { restaurants: data, keyword })
+        }
+    }).catch((err) => {
+        res.status(422).json(err)
+    })
 })
 
 app.listen(port, () => {
